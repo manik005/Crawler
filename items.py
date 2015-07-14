bf5b424b8d54983c63c486989ecd28b42f9c13bc
Manikandan S <manik.005@gmail.com>
2015-07-15 01:25:10 +0530

    updated code

--- items.py
+++ items.py
@@ -5,14 +5,25 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-
 import scrapy
+from scrapy.item import Item, Field
 
-class CrawlingItem(scrapy.Item):
-    model=scrapy.Field()
-    offer=scrapy.Field()
-    image=scrapy.Field()
-    standard_url=scrapy.Field()
 
-    
-   
+class TutorialItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+	#name = Field()
+    #pass
+    model=Field()
+    offer=Field()
+    image=Field()
+    standard_url=Field()
+    included_software=Field()
+    ram=Field()
+    ram_type=Field()
+    brand=Field()
+    part_number=Field()
+    model_id=Field()
+ #   processor=Field()
+ #   ram=Field()
+ #   hdd=Field()
